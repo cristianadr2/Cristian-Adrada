c741379bf1a8ce858f8e52c5807b89d7ede4ca02
lauvel19 <lgvelez_159@cue.edu.co>
2024-02-22 09:58:57 -0500

    ejemplos

---
+++ ejem.js
@@ -0,0 +1,33 @@
+//Ejercicio 1
+
+let edad = 9;
+
+edad >= 18 ? console.log("Es mayor de edad.."): console.log("Es menor de edad-.");
+
+
+//Ejercicio 2
+
+let numero = -3;
+
+numero > 0 ? console.log("numero positivo") : console.log(numero negativo);
+
+
+//Ejercicio 3
+
+let ocupacionCancha = true
+let nombreCancha = "Bernabeu";
+
+let mensaje = ocupacionCancha ? la cancha llamada ${nombreCancha} esta ocupada"
+
+
+
+
+
+
+
+
+
+
+
+
+
